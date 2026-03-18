39e44f0988827c602e51a278c28a43b320eeb31f
VICTOR EMANUEL DOS SANTOS ROCHA <victor.esrocha@senacsp.edu.br>
2026-03-18 11:38:48 -0300

    subindo atividades

---
+++ 14-objetosdoconsole/js/script.js
@@ -0,0 +1,16 @@
+//Exbibindo uma mensagem no avi
+console.log("Atenção: esta é uma mensagme de aviso");
+
+
+//Exibindo uma menagem de erro no console
+console.log("Ocorreu um erro");
+
+//Exibindo uma mensagem informativa no console
+console.info("Esta é uma mensagem informativa");
+
+// Criando um grupo de mensagem no console
+console.log("Mensagens de teste");
+console.log("Eta é uma mensagem de teste ");
+console.log(" Esta é outra mensagens de teste");
+console.groupEnd();
+ 
